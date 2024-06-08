db112ec71f913d454869b6605e2cadb74406debe
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-06-09 00:19:17 +0530

    All the main functionality completed

---
+++ src/components/Text.jsx
@@ -0,0 +1,25 @@
+import Draggable from "react-draggable";
+import { useState } from "react";
+
+const Text = () => {
+  const [editMode, setEditMode] = useState(false);
+  const [val, setVal] = useState("Double Click to Edit");
+
+  
+  return (
+    <Draggable>
+      {editMode ? (
+        <input
+          onDoubleClick={e => setEditMode(false)}
+          type="text"
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+      ) : (
+        <h1 onDoubleClick={(e)=> setEditMode(true)}>{val}</h1>
+      )}
+    </Draggable>
+  );
+};
+
+export default Text;
--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,12 +1,34 @@
+import { createRef, useState } from "react";
+import { Button } from "react-bootstrap";
 import { useSearchParams } from "react-router-dom";
+import Text from "../components/Text";
+import { exportComponentAsJPEG } from "react-component-export-image";
 
-const EditPage = () =>{
-    const [params] = useSearchParams();
-    return(
-        <div>
-            <img src={params.get("url")} alt="image" width="250px"/>
-        </div>
-    )
-}
+const EditPage = () => {
+  const [params] = useSearchParams();
+  const [count, setCount] = useState(0);
+  const memeRef = createRef();
+
+  const addText = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <div ref={memeRef} className="meme mt-5 mb-5" style={{width:"700px", border: "1px solid"}}>
+        <img src={params.get("url")} alt="image" width="250px" />
+        {Array(count)
+          .fill(0)
+          .map((e) => (
+            <Text />
+          ))}
+      </div>
+      <Button onClick={addText}>Add Text</Button>
+      <Button variant="sucess" onClick={e => exportComponentAsJPEG(memeRef)}>
+        Save
+      </Button>
+    </div>
+  );
+};
 
 export default EditPage;
